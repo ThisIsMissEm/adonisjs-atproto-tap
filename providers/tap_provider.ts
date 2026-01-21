3559ed10a0cbff5519462d24502740a429e5fc23
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2026-01-22 00:34:22 +0100

    Add support for LexIndexer, a lexicon aware indexer

--- providers/tap_provider.ts
+++ providers/tap_provider.ts
@@ -1,20 +1,18 @@
 import { RuntimeException } from '@adonisjs/core/exceptions'
-import { Tap, SimpleIndexer, TapChannel } from '@atproto/tap'
+import { Tap } from '@atproto/tap'
 import type { ApplicationService } from '@adonisjs/core/types'
 import type { TapProviderConfig } from '../src/types.js'
 import TapApi from '../src/api.js'
+import { ContainerProviderContract } from '@adonisjs/core/types/app'
 
 declare module '@adonisjs/core/types' {
   export interface ContainerBindings {
     'tap.client': Tap
-    'tap.indexer': SimpleIndexer
     'tap.api': TapApi
   }
 }
 
-export default class TapProvider {
-  private channel?: TapChannel
-
+export default class TapProvider implements ContainerProviderContract {
   constructor(protected app: ApplicationService) {}
 
   register() {
@@ -30,36 +28,9 @@ export default class TapProvider {
       return new Tap(config.url, config.config ?? {})
     })
 
-    this.app.container.singleton('tap.indexer', () => {
-      return new SimpleIndexer()
-    })
-
     this.app.container.singleton('tap.api', async (resolver) => {
       const client = await resolver.make('tap.client')
-      return new TapApi(client)
+      return new TapApi(client, this.app)
     })
-  }
-
-  async boot() {
-    const logger = await this.app.container.make('logger')
-    const indexer = await this.app.container.make('tap.indexer')
-
-    indexer.error((err) => logger.error(err, 'Tap indexer error'))
-  }
-
-  async start() {
-    if (this.app.getEnvironment() === 'web') {
-      const tap = await this.app.container.make('tap.client')
-      const indexer = await this.app.container.make('tap.indexer')
-
-      this.channel = tap.channel(indexer)
-      this.channel.start()
-    }
-  }
-
-  async shutdown() {
-    if (this.channel) {
-      await this.channel.destroy()
-    }
   }
 }
